fe670858340a407f5875587f1d18902a88c24804
SABARI S B <earc.ethereum@gmail.com>
2023-04-06 00:24:52 +0530

    6/4/23 commit

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { ReactDOM } from 'react-dom/client';
-import { Form, BrowserRouter as Router } from 'react-router-dom';
-import { ChainId, ThirdwebConfigProvider, ThirdwebProvider } from '@thirdweb-dev/react';
-
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import App from './App';
+import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
