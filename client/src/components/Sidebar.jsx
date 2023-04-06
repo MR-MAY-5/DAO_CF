e38a8a90bd870490080adc4699315a96d9b8128f
SABARI S B <earc.ethereum@gmail.com>
2023-04-06 10:29:21 +0530

    final-commit

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { logo, sun } from '../assets';
-import sabu from '../assets/arcai.png'
+
 import { navlinks } from '../constants';
 
 
