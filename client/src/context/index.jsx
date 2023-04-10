2fea36c7f1908ed32f328aa2fd9850a5aab2a0f8
SABARI S B <earc.ethereum@gmail.com>
2023-04-10 14:10:14 +0530

    Revert "Donation_updated"
    This reverts commit 0acf6f2c53b8a4025872af9d8ef97a565eec1eba.

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -9,7 +9,6 @@ const StateContext = createContext();
 export const StateContextProvider = ({ children }) => {
   const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
   const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
-  const { mutateAsync: donateToCampaign } = useContractWrite(contract, "donateToCampaign");
 
   const address = useAddress();
   const connect = useMetamask();
