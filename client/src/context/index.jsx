6720cc75da9a643d5f42aa25c04dde6ec5cc2fc9
SABARI S B <earc.ethereum@gmail.com>
2023-04-10 14:19:24 +0530

    Revert "Donation_updated"
    This reverts commit 0acf6f2c53b8a4025872af9d8ef97a565eec1eba.

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, createContext } from 'react';
 
-import { useAddress, useContract, useMetamask, useContractWrite, useContractRead } from '@thirdweb-dev/react';
+import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
 import { ethers } from 'ethers';
 import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 
@@ -9,7 +9,6 @@ const StateContext = createContext();
 export const StateContextProvider = ({ children }) => {
   const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
   const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
-  const { mutateAsync: donateToCampaign } = useContractWrite(contract, "donateToCampaign");
 
   const address = useAddress();
   const connect = useMetamask();
@@ -63,7 +62,7 @@ export const StateContextProvider = ({ children }) => {
   }
 
   const getDonations = async (pId) => {
-    const { donations } = useContractRead(contract, "getDonators", [pId])
+    const donations = await contract.call('getDonators', pId);
     const numberOfDonations = donations[0].length;
 
     const parsedDonations = [];
