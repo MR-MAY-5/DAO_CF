87240180b026a0240c676dc4423ed35da72b6132
SABARI S B <earc.ethereum@gmail.com>
2023-05-04 10:46:24 +0530

    contract read updated | undex.jsx

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, createContext } from 'react';
 
-import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
+import { useAddress, useContract, useMetamask, useContractWrite, useContractRead } from '@thirdweb-dev/react';
 import { ethers } from 'ethers';
 import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 
