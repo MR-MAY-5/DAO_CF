953fc4fa75942b2fb16ba3001f6b2ae93757e8c4
AKILAN S <akilanlakshmi2003@gmail.com>
2023-05-06 09:48:01 +0530

    create campaign updated _working

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Sepolia } from '@thirdweb-dev/chains'
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
 import { StateContextProvider } from './context';
@@ -10,7 +11,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ThirdwebProvider desiredChainId={ChainId.Sepolia}>
+  <ThirdwebProvider activeChain={Sepolia}>
     <Router>
       <StateContextProvider>
         <App />
