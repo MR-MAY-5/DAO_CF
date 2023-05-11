c83a5f2becaa0e6cf21a2a4bbddaa2afc46393f6
Bhoopesh S J <bhoopu67@gmail.com>
2023-05-11 12:59:41 +0530

    dark mode updated
    Need to update all pages for light and dark theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
 const App = () => {
   return (
     <section className='App'>
-    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+    <div className="relative sm:-8 p-4 bg-white dark:bg-[#13131a] min-h-screen flex flex-row">
       <div className="sm:flex hidden mr-10 relative">
         <Sidebar />
       </div>
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,38 +1,62 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import { log, sun } from '../assets';
-import { navlinks } from '../constants';
+import { log, sun } from "../assets";
+import { navlinks } from "../constants";
 
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
-  <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'} flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
+  <div
+    className={`w-[48px] h-[48px] rounded-[10px] ${
+      isActive && isActive === name && "bg-[#2c2f32]"
+    } flex justify-center items-center ${
+      !disabled && "cursor-pointer"
+    } ${styles}`}
+    onClick={handleClick}
+  >
     {!isActive ? (
       <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2" />
     ) : (
-      <img src={imgUrl} alt="fund_logo" className={`w-1/2 h-1/2 ${isActive !== name && 'grayscale'}`} />
+      <img
+        src={imgUrl}
+        alt="fund_logo"
+        className={`w-1/2 h-1/2 ${isActive !== name && "grayscale"}`}
+      />
     )}
   </div>
-)
+);
 
 const Sidebar = () => {
   const navigate = useNavigate();
-  const [isActive, setIsActive] = useState('dashboard');
+  const [isActive, setIsActive] = useState("dashboard");
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  function handleThemeSwitch() {
+    setTheme(theme === "dark" ? "light" : "dark");
+  }
 
   return (
     <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
-      <Link to="/">   
+      <Link to="/">
         <Icon styles="w-[52px] h-[52px] bg-[#ffffff]" imgUrl={log} />
       </Link>
 
       <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
         <div className="flex flex-col justify-center items-center gap-3">
           {navlinks.map((link) => (
-            <Icon 
+            <Icon
               key={link.name}
               {...link}
               isActive={isActive}
               handleClick={() => {
-                if(!link.disabled) {
+                if (!link.disabled) {
                   setIsActive(link.name);
                   navigate(link.link);
                 }
@@ -40,11 +64,12 @@ const Sidebar = () => {
             />
           ))}
         </div>
-
-        <Icon styles="bg-[#1c1c24] shadow-secondary" imgUrl={sun} />
+        <button onClick={handleThemeSwitch}>
+          <Icon styles="bg-[#1c1c24] shadow-secondary" imgUrl={sun} />
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
